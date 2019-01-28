21fa1b6d4759b33a733ac2c0da3cca433ab31a84
ZacLinz <zaclinzmeyer@gmail.com>
2019-01-28 15:19:52 -0600

    start project

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-alert('hello world');
+var repository = ['Bulbasaur', 'Ivysaur', 'Venusaur', 'Charmander', 'Charmeleon', 'Charizard'];
+var height = [2, 3, 6, 2, 3, 5];
+var type = [['Grass', ' Poison'],
+  ['Grass', ' Poison'],
+  ['Grass', ' Poison'],
+  ['Fire'],
+  ['Fire'],
+  ['Fire', ' Flying'],
+]
+for (var i = 0; i < repository.length; i++){
+  if (height[i] > 5){
+  document.write((repository[i]) + ' is height ' + (height[i]) + ' (that is pretty big) and has type(s) ' + (type[i]) + '. ' )
+}else{
+  document.write((repository[i]) + ' is height ' + (height[i]) + ' and has type(s) ' + (type[i]) + '. ')
+}
+}
