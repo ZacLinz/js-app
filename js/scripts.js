4f0c8c31d8e37649b1898c07879cdea6fbce37ca
ZacLinz <zaclinzmeyer@gmail.com>
2019-01-29 20:29:24 -0600

    change to functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,57 @@
-var repository =[
-{
-  name: 'Bulbasaur',
-  height: 2,
-  type: ['Grass', ' Poison']
-},
-{
-  name: 'Ivysaur',
-  height: 3,
-  type: ['Grass', ' Poison']
-},
-{
-  name: 'Venusaur',
-  height: 6,
-  type: ['Grass', ' Poison']
-},
-{
-  name: 'Charmander',
-  height: 2,
-  type: ['Fire']
-},
-{
-  name: 'Charmeleon',
-  height: 3,
-  type: ['Fire']
-},
-{
-  name: 'Charizard',
-  height: 5,
-  type: ['Fire', ' Flying']
-}
-]
-for (var i = 0; i < repository.length; i++){
-  if (repository[i].height > 5){
-  document.write((repository[i].name) + ' is height ' + (repository[i].height) + ' (THAT IS HUGE) and has type(s) ' + (repository[i].type) + '. ' )
-}else{
-  document.write((repository[i].name) + ' is height ' + (repository[i].height) + ' and has type(s) ' + (repository[i].type) + '. ' )
-}
-}
+
+var pokemonRepository = (function () {
+  var pokemon =[
+  {
+    name: 'Bulbasaur',
+    height: 2,
+    type: ['Grass', ' Poison']
+  },
+  {
+    name: 'Ivysaur',
+    height: 3,
+    type: ['Grass', ' Poison']
+  },
+  {
+    name: 'Venusaur',
+    height: 6,
+    type: ['Grass', ' Poison']
+  },
+  {
+    name: 'Charmander',
+    height: 2,
+    type: ['Fire']
+  },
+  {
+    name: 'Charmeleon',
+    height: 3,
+    type: ['Fire']
+  },
+  {
+    name: 'Charizard',
+    height: 5,
+    type: ['Fire', ' Flying']
+  }
+  ];
+
+  function add(pokemon){
+    pokemon.push(pokemon);
+  }
+
+  function getAll(){
+    return pokemon
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon){
+    if ( pokemon.height > 5){
+    document.write(pokemon.name + ' is height ' + pokemon.height + ' (THAT IS HUGE) and has type(s) ' + pokemon.type + '. ' )
+  }else{
+    document.write(pokemon.name + ' is height ' + pokemon.height + ' and has type(s) ' + pokemon.type + '. ' )
+  }
+})
